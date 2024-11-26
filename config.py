d0277781494c1430876c58017f378749b6e2a0bd
Guru Teja <108510733+Gurutejareddy9@users.noreply.github.com>
2024-11-26 13:20:27 +0530

    done

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
 # config.py
-OPENWEATHERMAP_API_KEY = '27a76472909b73ee8d9a5f1d5f3ef049'
+OPENWEATHERMAP_API_KEY = 'ENTER YOUR API HERE'
 OPENWEATHERMAP_API_URL = 'http://api.openweathermap.org/data/2.5/weather'
